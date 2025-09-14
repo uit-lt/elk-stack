78fd1bc3d58c81665d140659e9b085f0806c9ee3
Tan Nguyen <tannp27@gmail.com>
2025-09-14 08:49:08 +0700

    feat: add tests

---
+++ spark/tests/test_top_relationships.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+
+def test_top_relationships():
+    spark = SparkSession.builder \
+        .appName("TopRelationships") \
+        .config("spark.es.nodes", "elasticsearch") \
+        .config("spark.es.port", "9200") \
+        .getOrCreate()
+
+    df = spark.read.format("org.elasticsearch.spark.sql").load("2_connections_data_300k")
+
+    df.groupBy("relationship").count().orderBy("count", ascending=False).show(3)
+
+    spark.stop()
+
+if __name__ == "__main__":
+    test_top_relationships()
